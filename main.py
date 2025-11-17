b353b6f7f9762e316f474f2541ab4273e966c263
prasuk-jain67 <prasuk0129@gmail.com>
2025-11-17 14:33:01 +0000

    final commit

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from stats import get_num_words,get_char_count
+from stats import get_num_words,get_char_count,return_sorted
+import sys
 def get_book_text(filePath):
     with open(filePath,'r', encoding='utf-8') as f:
         file_contents = f.read()
     return file_contents
-def main():
-    book_path = "books/frankenstein.txt"
+def main(book_path):
     text = get_book_text(book_path)
     return text
 
 if __name__ == "__main__":
-    text=main()
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path =sys.argv[1]
+    text=main(path)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     print(f"Found {get_num_words(text)} total words")
-    char_frequency= get_char_count(text)
-    print(char_frequency)
+    print("--------- Character Count -------")
+    char_frequency= return_sorted(get_char_count(text))
+    for dict in char_frequency:
+        print(f"{dict["char"]}: {dict["count"]}")
+        
+    print("============= END ===============")
+   
+    
    
--- stats.py
+++ stats.py
@@ -9,6 +9,13 @@ def get_char_count(text):
         else:
             char_frequency[symbol.lower()]=1
     return char_frequency
-        
+def sort_on(freq):
+    return freq["count"]
+def return_sorted(freq):
+    lst=[]
+    for val in freq:
+        lst.append({"char":val ,"count":freq[val]})
+    lst.sort(reverse=True,key=sort_on) 
+    return lst
         
         
